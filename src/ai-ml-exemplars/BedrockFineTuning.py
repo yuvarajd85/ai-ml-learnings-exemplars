2e8629dc022c5202e69527d3d228c23f5ad3e026
yuvarajd85 <yuvaraj.d85@gmail.com>
2025-02-25 10:42:44 -0500

    join query spec

--- src/ai-ml-exemplars/BedrockFineTuning.py
+++ src/ai-ml-exemplars/BedrockFineTuning.py
@@ -10,6 +10,8 @@ from dotenv import load_dotenv
 import boto3
 import json
 
+from sympy.physics.units import temperature
+
 load_dotenv()
 
 
@@ -30,22 +32,75 @@ def main():
         "filters" : [{"column" : "id", "operator": "IN", "value": "1,23,5"}]
     }
 
+    join_query_sepc = {
+	"base_table" : {
+			"table" : "db.table_emp",
+			"cols" : ["name","id"],
+			"filters" : [{"column" : "id", "operator": "IN", "value": "1,23,5"}]
+		},
+	"join_tables" : [
+		{
+			"table" : "db.table_emp",
+			"cols" : ["id","dept","division"],
+			"how" : "inner",
+			"leftTable" : "db.table_dept",
+			"leftKeys" : ["id"],
+			"rightKeys" : ["id"],
+			"filters" : [{"column" : "dept", "operator": "IN", "value": "assembly,painting"}]
+		},
+		{
+			"table" : "db.table_emp",
+			"cols" : ["id","service_start_date","service_end_date","service_dept"],
+			"how" : "inner",
+			"leftTable" : "db.table_service",
+			"leftKeys" : ["id"],
+			"rightKeys" : ["id"],
+			"filters" : [{"column" : "service_start_date", "operator": "<", "value": ":current-date"}]
+		}
+	]
+}
     query_spec_str = json.dumps(query_spec)
 
-    prompt = f"Generate the sql using the following query spec: {query_spec_str}"
+    # prompt = f"Generate the sql using the following query spec: {query_spec_str}"
+
+    prompt = """
+    Generate the sql using the following query spec: 
+    {
+        "table" : "db.table_a",
+        "cols" : ["name","id"],
+        "filters" : [{"column" : "id", "operator": "IN", "value": "1,23,5"}]
+    }
+    """
+
+    body = json.dumps({
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 1000,
+        "messages" : [
+            {
+                "role" : "user",
+                "content" : [{"type": "text", "text": prompt}]
+            }
+        ],
+        "temperature" : 0.5
+    })
+
+    prompt_request = {
+        "body" : body,
+        "modelId" : "anthropic.claude-3-5-sonnet-20240620-v1:0"
+    }
 
     bedrock_client = get_client(service_name="bedrock-runtime", access_dict=access_dict)
 
     # response = bedrock_client.list_foundation_models()
 
     response = bedrock_client.invoke_model(
-        modelId="meta.llama3-3-70b-instruct-v1:0",
-        contentType="string",
-        accept='string',
-        body=prompt
+        modelId=prompt_request['modelId'],
+        body=prompt_request['body']
     )
 
-    print(response)
+    response_json = json.loads(response.get('body').read())
+    response_text = response_json.get('content')[0].get('text')
+    print(response_text)
 
 def get_client(service_name : str, access_dict : Dict):
     return boto3.Session(aws_access_key_id=access_dict.get("access_key"), aws_secret_access_key=access_dict.get("secret_access_key"),region_name="us-east-1").client(service_name=service_name)
